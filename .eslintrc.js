18264c184fed53b8cfd0ea56f94c6bee4c9b7ed5
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-24 23:30:27 +0300

    chore: Multiple changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-  // eslint-disable-next-line no-warning-comments -- Wait for eslint-plugin-misc update
-  // fixme - Add "./node_modules/real-config/eslint/packages/type-essentials"
   extends: [
     "./node_modules/real-config/eslint",
+    "./node_modules/real-config/eslint/packages/type-essentials",
     "./node_modules/real-config/eslint/special-locations",
     "./.eslintrc.overrides",
     "./.eslintrc.rule-overrides",
